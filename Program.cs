60f4e3359bed5df5cd057468f255d0abf9d27c52
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-01 20:15:47 -0400

    bugs fixed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace variablesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string store = "Target";
+            int pairsBought;
+            char amp = '&';
+            bool doAgain;
+            double moneyLeft = 11.23;
+            decimal owedDamages = 974.3M;
+
+            pairsBought = 12;
+            doAgain = true;
+
+            Console.WriteLine("I just went to " + store + "," + " I got " + pairsBought + " pairs of shoes! I just got paid but there's only $" + moneyLeft + " left in my account " + amp + " I paid $" + owedDamages + " ! Would I go again? " + doAgain);
+        }
+    }
+}
